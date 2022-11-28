c3282995ad1f266345ece7e668dfc63e9540a3a5
Maks Danylchuk <maxitosm00@gmail.com>
2022-11-28 06:36:37 +0200

    Add view index

--- kitchen_service/views.py
+++ kitchen_service/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen_service.models import DishType, Cook, Ingredient, Dish
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_dish_types = DishType.objects.count()
+    num_cooks = Cook.objects.count()
+    num_ingredients = Ingredient.objects.count()
+    num_dishes = Dish.objects.count()
+
+    context = {
+        "num_dish_types": num_dish_types,
+        "num_cooks": num_cooks,
+        "num_ingredients": num_ingredients,
+        "num_dishes": num_dishes,
+    }
+
+#    return render(request, "kitchen_service/index.html", context=context)
