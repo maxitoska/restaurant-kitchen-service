8caed827563e29d1a451855d8a4635c5c0041b23
Maks Danylchuk <maxitosm00@gmail.com>
2022-11-28 08:17:42 +0200

    Add new templates: cook_list, cook_detail, dish_list, dish_detail and other for my views, add with help of varible template_name in models, add information to cook_list template

--- kitchen_service/views.py
+++ kitchen_service/views.py
@@ -25,34 +25,40 @@ def index(request):
 class DishTypeListView(generic.ListView):
     model = DishType
     queryset = DishType.objects.order_by("name")
+    template_name = "kitchen_service/dish_type_list.html"
     paginate_by = 5
 
 
 class CookListView(generic.ListView):
     model = Cook
     paginate_by = 5
+    template_name = "kitchen_service/cook_list.html"
 
 
 class CooksDetailView(generic.DetailView):
     model = Cook
     paginate_by = 5
-
+    template_name = "kitchen_service/cook_detail.html"
 
 class IngredientListView(generic.ListView):
     model = Ingredient
     paginate_by = 5
+    template_name = "kitchen_service/ingredient_list.html"
 
 
 class IngredientDetailView(generic.DetailView):
     model = Ingredient
     paginate_by = 5
+    template_name = "kitchen_service/ingredient_detail.html"
 
 
 class DishListView(generic.ListView):
     model = Dish
     paginate_by = 5
+    template_name = "kitchen_service/dish_list.html"
 
 
 class DishDetailView(generic.DetailView):
     model = Dish
     paginate_by = 5
+    template_name = "kitchen_service/dish_detail.html"
