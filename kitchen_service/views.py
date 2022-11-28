12f490fc7865baf0ba4c3356b4dc60d82f056db7
Maks Danylchuk <maxitosm00@gmail.com>
2022-11-28 08:08:16 +0200

    Add new urls: dish_types, cooks, ingredients, dishes in pass i use Class-based views, also add same views as CookListView, CooksDetailView for all models, and also add base html file and new includes in directory Templates with 2 files pagination and sidebar

--- kitchen_service/views.py
+++ kitchen_service/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from kitchen_service.models import DishType, Cook, Ingredient, Dish
 
@@ -21,3 +22,37 @@ def index(request):
     return render(request, "kitchen_service/index.html", context=context)
 
 
+class DishTypeListView(generic.ListView):
+    model = DishType
+    queryset = DishType.objects.order_by("name")
+    paginate_by = 5
+
+
+class CookListView(generic.ListView):
+    model = Cook
+    paginate_by = 5
+
+
+class CooksDetailView(generic.DetailView):
+    model = Cook
+    paginate_by = 5
+
+
+class IngredientListView(generic.ListView):
+    model = Ingredient
+    paginate_by = 5
+
+
+class IngredientDetailView(generic.DetailView):
+    model = Ingredient
+    paginate_by = 5
+
+
+class DishListView(generic.ListView):
+    model = Dish
+    paginate_by = 5
+
+
+class DishDetailView(generic.DetailView):
+    model = Dish
+    paginate_by = 5
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -16,8 +16,58 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from kitchen_service.views import index
+from kitchen_service.views import (
+    index,
+    DishTypeListView,
+    CookListView,
+    CooksDetailView,
+    IngredientListView,
+    IngredientDetailView,
+    DishListView,
+    DishDetailView
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "dish_types/",
+        DishTypeListView.as_view(),
+        name="dish-type-list",
+    ),
+
+    path(
+        "cooks/",
+        CookListView.as_view(),
+        name="cook-list",
+    ),
+
+    path(
+        "cooks/<int:pk>/",
+        CooksDetailView.as_view(),
+        name="cooks-detail",
+    ),
+
+    path(
+        "ingredients/",
+        IngredientListView.as_view(),
+        name="ingredient-list",
+    ),
+
+    path(
+        "ingredient/<int:pk>/",
+        IngredientDetailView.as_view(),
+        name="ingredient-detail",
+    ),
+    path(
+        "dishes/",
+        DishListView.as_view(),
+        name="dish-list",
+    ),
+
+    path(
+        "dish/<int:pk>/",
+        DishDetailView.as_view(),
+        name="dish-detail",
+    ),
 ]
+
