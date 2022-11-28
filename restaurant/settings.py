52fad6dd7e564f62e606c3b0e6029c899f6b7290
Maks Danylchuk <maxitosm00@gmail.com>
2022-11-28 06:43:37 +0200

    Add in settings.py installed_apps, auth_user_model, time_zone, staticfiles_dirs, static_root and add new directory static

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kitchen_service',
 ]
 
 MIDDLEWARE = [
@@ -102,13 +103,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "kitchen_service.Cook"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
@@ -120,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
