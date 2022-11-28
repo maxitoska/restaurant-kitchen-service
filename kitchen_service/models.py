00b34e38c67489eec379925f4599dc5644f9cbd7
Maks Danylchuk <maxitosm00@gmail.com>
2022-11-28 06:25:54 +0200

    Add model Ingredient with Many-to-many connection to Dish

--- kitchen_service/models.py
+++ kitchen_service/models.py
@@ -14,9 +14,16 @@ class Cook(AbstractUser):
         verbose_name_plural = "cooks"
 
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    count = models.IntegerField()
+    price = models.IntegerField()
+
+
 class Dish(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
     price = models.IntegerField()
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
     cooks = models.ManyToManyField(Cook, related_name="dishes")
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
