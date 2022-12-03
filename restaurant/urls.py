4e840ac3c5420fe756cb2f6abdde65e85979120a
Maks Danylchuk <maxitosm00@gmail.com>
2022-12-03 11:14:01 +0200

    Changed requirements with pip version

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,5 +1,4 @@
 """restaurant URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
@@ -13,9 +12,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("admin/", admin.site.urls),
+    path("", include("kitchen_service.urls", namespace="kitchen_service")),
+    path("accounts/", include("django.contrib.auth.urls")),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+app_name = "kitchen_service"
